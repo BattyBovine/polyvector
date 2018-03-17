5a6326a8cf99a85f6d3ff90b9ae64f17d1372e47
Jamie Greunbaum <BattyBovine+github@battybovine.com>
2018-03-17 14:20:45 -0400

    Added include guards to "register_types.h" that were somehow missing and should probably be included just in case.

--- register_types.h
+++ register_types.h
@@ -1,2 +1,7 @@
+#ifndef REGISTER_POLYVECTOR_TYPES_H
+#define REGISTER_POLYVECTOR_TYPES_H
+
 void register_polyvector_types();
 void unregister_polyvector_types();
+
+#endif	// REGISTER_POLYVECTOR_TYPES_H
